016e5e540fb5ff5ca1d639da9b62308805ceb4d9
johnskyi <johnskyi2@yandex.ru>
2021-07-29 11:26:34 +0300

    added 12 part

---
+++ 12_SpringBootWebDevelopment/BookLibrary/target/classes/static/js/main.js
@@ -0,0 +1,75 @@
+$(function(){
+
+    const appendTask = function(data){
+        var taskCode = '<a href="#" class="task-link" data-id="' +
+            data.id + '">' + data.task + '</a><br>';
+        $('#task-list')
+            .append('<div>' + taskCode + '</div>');
+    };
+
+    //Loading books on load page
+//    $.get('/books/', function(response)
+//    {
+//        for(i in response) {
+//            appendBook(response[i]);
+//        }
+//    });
+
+    //Show adding task form
+    $('#show-add-task-form').click(function(){
+        $('#task-form').css('display', 'flex');
+    });
+
+    //Closing adding task form
+    $('#task-form').click(function(event){
+        if(event.target === this) {
+            $(this).css('display', 'none');
+        }
+    });
+
+    //Getting task
+    $(document).on('click', '.task-link', function(){
+        var link = $(this);
+        var taskId = link.data('id');
+        $.ajax({
+            method: "GET",
+            url: '/tasks/' + taskId,
+            success: function(response)
+            {
+                var code = '<span>Год выпуска:' + response.year + '</span>';
+                link.parent().append(code);
+            },
+            error: function(response)
+            {
+                if(response.status == 404) {
+                    alert('Книга не найдена!');
+                }
+            }
+        });
+        return false;
+    });
+
+    //Adding task
+    $('#save-task').click(function()
+    {
+        var data = $('#task-form form').serialize();
+        $.ajax({
+            method: "POST",
+            url: '/tasks/',
+            data: data,
+            success: function(response)
+            {
+                $('#task-form').css('display', 'none');
+                var task = {};
+                task.id = response;
+                var dataArray = $('#task-form form').serializeArray();
+                for(i in dataArray) {
+                    task[dataArray[i]['name']] = dataArray[i]['value'];
+                }
+                appendTask(task);
+            }
+        });
+        return false;
+    });
+
+});
